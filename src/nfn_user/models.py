95640cdbdbd723fbc5cef9c678550eeea8aa1335
Maarvius <murat.ertek@stu.bilgi.edu.tr>
2015-12-13 17:56:03 +0200

    Fix signup view

--- src/nfn_user/models.py
+++ src/nfn_user/models.py
@@ -2,9 +2,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import ModelForm
 
 class C_Owner(models.Model):
 	profile_model_referance = models.OneToOneField(User,on_delete=models.CASCADE)
 	website = models.URLField('Website', max_length=200,)
 	company_name = models.CharField('Company Name', max_length=200)
 	company_address = models.TextField('Company Address')
+
+class UserModelForm(ModelForm):
+	class Meta:
+		model = User
+		fields = ['username','password','first_name','last_name','email']
--- src/nfn_user/views.py
+++ src/nfn_user/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from .models import C_Owner, UserModelForm
 from django.contrib.auth.models import User
+from .models import C_Owner, UserModelForm
 from django.forms import inlineformset_factory
 
 def cOwnerSignUpView(request):
 	userForm = UserModelForm()
 	user = User()
-	COwnerFormSet = inlineformset_factory(User, C_Owner, can_delete=False)
+	COwnerFormSet = inlineformset_factory(User, C_Owner, can_delete=False, fields=('website','company_name','company_address'))
 	if request.method == 'POST':
 		user = COwnerFormSet(request.Post)
 		created_user = user.save()
