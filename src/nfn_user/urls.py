683f1e472c5d277a8b8994efca1d3169a462b502
Altay Aydemir <altayaydemir@gmail.com>
2015-12-20 01:28:26 +0200

    Start building FrontEnd side

--- src/nfn_user/urls.py
+++ src/nfn_user/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
-    url(r'^signup/cowner/$', views.cOwnerSignUpView),
-    url(r'^signup/applicant/$', views.applicantSignUpView),
-    url(r'^login/$', views.loginView),
-    url(r'^logout/$', views.logoutView)
+    url(r'^signup/cowner/$', views.cOwnerSignUpView, name='signup_cowner'),
+    url(r'^signup/applicant/$', views.applicantSignUpView, name='signup_applicant'),
+    url(r'^login/$', views.loginView, name='login'),
+    url(r'^logout/$', views.logoutView, name='logout')
 ]
 
