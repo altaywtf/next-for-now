100a85b9f3ab96c4b6dd1c6168ccd0320d2dacc3
Altay Aydemir <altayaydemir@gmail.com>
2015-12-20 10:44:31 +0200

    Create/Read/Delete forms updated

--- src/nfn_contests/forms.py
+++ src/nfn_contests/forms.py
@@ -4,26 +4,25 @@ from .models import Contest, Submission
 class ContestForm(forms.ModelForm):
 	class Meta:
 		model = Contest
-		fields = ['title', 'category', 'description', 'details', 'award', 'date_started', 'date_deadline']
+		exclude = ['owner', 'is_approved']
 		widgets = {
             'date_started': forms.SelectDateWidget(),
             'date_deadline': forms.SelectDateWidget(),
+            'details': forms.Textarea(attrs={'rows': 3}),
         }
 
 class SubmissionForm(forms.ModelForm):
   class Meta:
     model = Submission
     fields = ['a_names', 'a_details', 's_details', 's_file']
-
+    widgets = {
+          'a_details': forms.Textarea(attrs={'rows': 3}),
+          's_details': forms.Textarea(attrs={'rows': 3}),
+    }
 
 '''
-        ____                           
-   _||__|  |  ______   ______   ______ 
-  (        | |      | |      | |      |
-  /-()---() ~ ()--() ~ ()--() ~ ()--()
 
-
-class FeedBackCreatinForm(forms.Form):
+class FeedBackCreationForm(forms.Form):
 	class Meta:
 		model = Submission
 		fields = ['feedback', 'is_winner']
