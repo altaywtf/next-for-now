e533583b52eddcf34cc794ab0e9869f865dd1fae
Maarvius <murat.ertek@stu.bilgi.edu.tr>
2015-12-27 00:25:41 +0200

    Add password change urls

--- src/nfn_user/urls.py
+++ src/nfn_user/urls.py
@@ -10,12 +10,14 @@ urlpatterns = [
     url(r'^login/$', views.loginView, name='login'),
     url(r'^logout/$', views.logoutView, name='logout'),
     url(r'^settings/$', views.userChangeView, name='settings'),
-    url(r'^password/reset/$', 'django.contrib.auth.views.password_reset', {'post_reset_redirect': '/user/password/reset/done/$'},
+    url(r'^password/reset/$', 'django.contrib.auth.views.password_reset', {'post_reset_redirect': r'done/$'},
     	name="password_reset"),
     url(r'^password/reset/done/$', 'django.contrib.auth.views.password_reset_done'),
-    url(r'^user/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 
+    url(r'^password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 
         'django.contrib.auth.views.password_reset_confirm', 
         {'post_reset_redirect' : '/user/password/done/'}),
-    url(r'^user/password/done/$', 
+    url(r'^password/done/$', 
         'django.contrib.auth.views.password_reset_complete'),
+    url(r'^password/change/$', 'django.contrib.auth.views.password_change', {'post_change_redirect' : r'done/'}, name="password_change"),
+    url(r'^password/change/done/$', 'django.contrib.auth.views.password_change_done')
 ]
